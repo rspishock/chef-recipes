4a7ffc35da59e7a594d028686014eb8dde84aa15
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-09-17 15:10:54 -0400

    Add setup.rb recipe.

---
+++ setup.rb
@@ -0,0 +1,14 @@
+package 'tree' do
+    action :install
+end
+
+package 'ntp' do
+    action :install
+end
+
+file '/etc/motd' do
+    content 'This server is the property of Ryan Spishock.'
+    action :create
+    owner 'root'
+    group 'root'
+end
