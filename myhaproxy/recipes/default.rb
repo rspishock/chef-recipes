6f03aa487d5e33219a50a4f05201602e51205fcc
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-10-18 15:50:20 -0400

    Update myhaproxy.rn

--- myhaproxy/recipes/default.rb
+++ myhaproxy/recipes/default.rb
@@ -7,25 +7,23 @@
 haproxy_install 'package'
 
 haproxy_frontend 'http-in' do
-  bind '*:80'
-  default_backend 'servers'
+	bind '*:80'
+	default_backend 'servers'
+end
+
+all_web_nodes = search('node', "role:web AND chef_environment:#{node.chef_environment}")
+
+servers = []
+
+all_web_nodes.each do |web_node|
+	server = "#{web_node['hostname']} #{web_node['ipaddress']}:80 maxconn32"
+	servers.push(server)
 end
 
 haproxy_backend 'servers' do
-  server [
-    'web1 192.168.10.43:80 maxconn 32', 
-    'web2 192.168.10.44:80 maxconn 32'
-  ]
+	server servers
+end
 
-  haproxy_backend 'servers' do
-    server [
-      'web1 192.168.10.43:80 maxconn 32',
-      # 'web2 192.168.10.44:80 maxconn 32'
-    ]
-    # notifies :reload, 'haproxy_service[haproxy]' :immediately
-    # https://github.com/sous-chef/haproxy/issues/274
-  end
-  
-  haproxy_service 'haproxy' do 
-    subscribes :reload, 'template[/etc/haproxy/haproxy.cfg]', :immediately
-  end
+haproxy_service 'haproxy' do
+	subscribes :reload, 'template[/etc/haproxy/haproxy.cfg]', :immediately
+end
